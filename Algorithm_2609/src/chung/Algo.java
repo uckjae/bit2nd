bc8257159eb3dcb32fa0c7c206262f78271cdf0f
euna.chung <local456a@gmail.com>
2019-09-26 20:17:14 +0900

    알고리즘

--- Algorithm_2609/src/chung/Algo.java
+++ Algorithm_2609/src/chung/Algo.java
@@ -1,5 +1,25 @@
 package chung;
 
 public class Algo {
+	public static void main(String[] args) {
+	
+			choi(24, 18);
+		
+		}	
 
+
+
+	static int choi(int x,int y) {
+		int max =1; 
+		int range = Math.min(x, y); 
+		for(int i =1;i<= range; i++) {
+			if(x%i == 0 && y%i == 0) {
+				max = i;	
+		}
+			
+	}
+		 System.out.println(max);
+	return max;
+}
+	
 }
