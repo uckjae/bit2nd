6e8dc5acaaf5a33ac22d04e8810c764c070d62d5
uckjae.lee <uckjae@naver.com>
2019-09-27 17:33:20 +0900

    0927 기본틀

---
+++ Bank/src/lee/Transaction.java
@@ -0,0 +1,9 @@
+package lee;
+
+public class Transaction {
+	private String transactionDate;
+	private String tracsactionTime;
+	private String kind;
+	private long amount;
+	private long balance;
+}
