746b474b1316f49a57568dd7717a26e69bb723a1
chungeuna <local456a@gmail.com>
2019-09-30 04:30:24 +0900

    별찍기

---
+++ Algoritm_2443_2444_2445/src/chung/Star2443.java
@@ -0,0 +1,20 @@
+package chung;
+
+import java.util.Scanner;
+
+public class Star2443 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i < num ; i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0;j < 2*(5-i)-1 ;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
