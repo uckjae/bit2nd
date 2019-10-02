5ac434ea0c396c1f0f792432ad4aa2c2287f5e3c
원보연 <qhdus114@gmail.com>
2019-10-02 09:27:33 +0900

    1

--- Algorithm_2839/src/won/Main_2839.java
+++ Algorithm_2839/src/won/Main_2839.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Main_2839 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int sugar = scan.nextInt();
+		int sugar = scan.nextInt(); // 41
 		int sugar5kg = 5;
 		int sugar3kg = 3;
 		int delivery = 0;
-		if(sugar % sugar5kg == 0) {
-			delivery = sugar / sugar5kg;
+		if(sugar % sugar5kg == 0) { // 1 X
+			delivery = sugar / sugar5kg; 
 			return;
 		}else {
-			int quotient = sugar / sugar5kg;
-			for(int i = quotient; i > 0; i--) {
-				int rest = sugar - (i * 5);
-				if(rest % sugar3kg == 0) {
-					int j = rest / sugar3kg;
-					delivery = i + j;
-					System.out.println("ÃÖ¼Ò°ª : " + delivery);
+			int quotient = sugar / sugar5kg; // ³ª´« ¸ò  // 8
+			for(int i = quotient; i > 0; i--) { // i´Â ¸ò // 8 -> 7
+				int rest = sugar - (i * 5); // ¸ò*5 = 40 - 41 // ³ª¸ÓÁö 1 - > 6
+				if(rest % sugar3kg == 0) { // 1 X // 6 O
+					int j = rest / sugar3kg; 
+					delivery = i + j; // i(5kg) = 7, j(3kg) = 2 // 7 + 2 
+					System.out.println("ÃÖ¼Ò°ª : " + delivery); // 9
 					return;
 				}
 			}
---
+++ Algoritm_2443_2444_2445/src/won/Main_2444.java
@@ -0,0 +1,31 @@
+package won;
+
+
+import java.util.Scanner;
+
+public class Main_2444 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		//System.out.print("출력할 줄 수를 입력하세요 : ");
+		int line = scan.nextInt();
+		for(int i = 1; i <= 2 * line - 1; i++) {
+			if(i <= line) {
+				for(int j = 1; j <= line - i; j++) {
+					System.out.print(" ");
+				}
+				for(int j = 1; j <= i * 2 -1; j++) {
+					System.out.print("*");
+				}
+			}else {
+				int down = i - line + 1;
+				for(int j = 1; j <= down - 1; j++) {
+					System.out.print(" ");
+				}
+				for(int j = down; j <= 2 * line - down; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
