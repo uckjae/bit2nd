e791b039fc7e9e8c4cad705434f8e7aae838f47d
ILGYEOM <chdl1229@gmail.com>
2019-10-07 23:13:17 +0900

    최소공배수 최대공약수 완료

--- Algorithm_2609/src/kim/max_min.java
+++ Algorithm_2609/src/kim/max_min.java
@@ -10,27 +10,25 @@ public class max_min {
 		int d = 0;
 		int min = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("큰 값을 입력 해주세요.");
+		System.out.println("첫 번째 숫자 입력.");
 		int a = Integer.parseInt(sc.nextLine());
-		System.out.println("작은 값을 입력 해주세요.");
+		System.out.println("두 번째 숫자 입력.");
 		int b = Integer.parseInt(sc.nextLine());
 
-		for (int i = 2; i < 100; i++) { // 소인수 분해 처럼 2 부터 증가해서 if문을 탄다
+		
+		
+		
+		for (int i = 2; i < 100; i++) {// 소인수 분해 처럼 2 부터 증가해서 if문을 탄다
+			
+			
 			while(a % i == 0 && b % i == 0) { // a , b가 i랑 나눠서 나머지가 0이라면
-				mok = 1*i; 
-
+				mok =1*i; 
+				break;
 			}
 		}
 		System.out.println("최대 공약수 : " + mok);
 
-		c = a / mok; // 최대공약수로 나눈 몫을 계산
-		d = b / mok;
-
-		min = mok * c * d; // 최소공배수 식
-
+		min = (a * b) / mok;
 		System.out.println("최소공배수 : " + min);
-		
-		if(a >)
-		
 	}
 }
