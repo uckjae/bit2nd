f8d8670aae97f86332e97d6aa709993207fc1dfa
kyungyeul.Choi <dnjsvltm201311681@gmail.com>
2019-10-07 18:06:53 +0900

    소인수분해

---
+++ Algorithm_11653/src/choi/Soinsubunha.java
@@ -0,0 +1,24 @@
+package choi;
+
+import java.util.Scanner;
+
+public class Soinsubunha {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("ют╥б:");
+		int n = sc.nextInt();
+		int num=0;
+		
+		while (n != 1) {
+			for (int i = 2; i <= n; i++) {
+				if (n % i == 0) {
+					n = n/i;
+					num=i;
+					System.out.println(num);
+					break;
+				}
+			}
+		}
+	}
+}
