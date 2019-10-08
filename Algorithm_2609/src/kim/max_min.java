85769f27e7db4e2f347aaccf7c87e28af26a8c85
iLGyeom.KIM <electric____@naver.com>
2019-10-08 17:38:29 +0900

    최대공약수 백준포맷

--- Algorithm_2609/src/kim/max_min.java
+++ Algorithm_2609/src/kim/max_min.java
@@ -6,29 +6,27 @@ public class max_min {
 
 	public static void main(String[] args) {
 		int mok = 1;
-		int c = 0;
-		int d = 0;
 		int min = 0;
+		int a = 0;
+		int b = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("첫 번째 숫자 입력.");
-		int a = Integer.parseInt(sc.nextLine());
-		System.out.println("두 번째 숫자 입력.");
-		int b = Integer.parseInt(sc.nextLine());
 
-		
-		
-		
-		for (int i = 2; i < 100; i++) {// 소인수 분해 처럼 2 부터 증가해서 if문을 탄다
-			
-			
-			while(a % i == 0 && b % i == 0) { // a , b가 i랑 나눠서 나머지가 0이라면
-				mok =1*i; 
+		String input = sc.nextLine();
+
+		String[] strs = input.split(" ");
+		a = Integer.parseInt(strs[0]);
+		b = Integer.parseInt(strs[1]);
+
+		for (int i = 2; i < a * b; i++) {// 소인수 분해 처럼 2 부터 증가해서 if문을 탄다
+
+			while (a % i == 0 && b % i == 0) { // a , b가 i랑 나눠서 나머지가 0이라면
+				mok = 1 * i;
 				break;
 			}
 		}
-		System.out.println("최대 공약수 : " + mok);
+		System.out.println(mok);
 
 		min = (a * b) / mok;
-		System.out.println("최소공배수 : " + min);
+		System.out.println(min);
 	}
 }
