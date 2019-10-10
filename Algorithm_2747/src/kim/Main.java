cac84aa7f726f801ab4c0a33961a975fbc3cba6c
iLGyeom.KIM <electric____@naver.com>
2019-10-10 18:03:45 +0900

    주석완료

--- Algorithm_2747/src/kim/Main.java
+++ Algorithm_2747/src/kim/Main.java
@@ -5,10 +5,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int a = 0;
-		int b = 1;
-		int sum = 0;
+		int num = sc.nextInt(); 
+		int a = 0; //0부터 
+		int b = 1; //1부터 
+		int sum = 0; //합을 저장할곳
 		
 		for(int i=0;i<num; i++) { // 입력한 숫자만큼 반복 작업
 			
--- Algorithm_9506/src/kim/Main.java
+++ Algorithm_9506/src/kim/Main.java
@@ -1,34 +1,41 @@
 package kim;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int save =0;
+		String input = null; //문자를 받을곳 
+		ArrayList<Integer> arr = new ArrayList<Integer>(); //리스트 생성 
 		
-		if(n == -1)  {
-			return;
-		}		
-		
-		System.out.print(n + "=");
-		for(int i=1;i<n;i++) {
-			if(n % i == 0) {
-				save +=i;
-				System.out.print(i + " + ");
-				//System.out.print("+");	
+		if(!(input.equals("-1"))) { //input에 들어간 것이 -1이 아니면 
+			for(int i=0;i<arr.size();i++) { //arr 리스트를 돌아서
+				arr.add(Integer.parseInt(input)); //리스트에 숫자타입으로 저장
+			}
+		}
+		StringBuilder sb = new StringBuilder(); //변경 가능하고 동기화를 하지않는 char[] 객체
+		for(Integer arrs : arr) { // 개선된 for문으로 저장한 리스트를 확인하며
+		int save=0;
+			String etc = ""; //공백을 넣고 
+			for(int i=1;i<arrs;i++) { //완전수를 인지 확인하기위해 1 부터 약수를 확인하고
+			if(arrs % i == 0) { //나머지가 0이고 약수가 맞으면 
+				etc += i + " + "; //etc 문자열에 저장
+					save +=i; //약수의 합을 저장
+				if (save > arrs)  //합이 리스트 숫자보다 크면 
+					break; //멈춘다.
 			}
 			
 			
 		}
-		if(n < save) {
-			System.out.println(n + "is NOT Perfect");
+		if(save == arrs) { //합과 리스트에 있는 숫자와 같으면 
+			sb.append(String.format("%d = %s", arrs,etc.substring(0,etc.length()-3)));
+			//StringBuilder에 지정된 위치에 값을 대입해서 문자열을 만든다. etc의 문자열 길이부터 문자열 길이에서 -3번째까지
+		}else { //합과 리스트에 숫자가 같지 않으면  
+			sb.append(String.format("%d is NOT perfect.", arrs));
+			//문자열을 출력 
 		}
-		
-		
+		}
 	}
 
 	}
