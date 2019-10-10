7158e25f6a94a0b0a2e263c59909161d11025250
kyungyeul.Choi <dnjsvltm201311681@gmail.com>
2019-10-10 18:21:23 +0900

    ㄹㄹ

---
+++ Algorithm_9506/src/CHOI/Main.java
@@ -0,0 +1,25 @@
+package CHOI;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int num=sc.nextInt();
+		int k=(num/2);
+		for(int i=1; i<=k; i++) {
+			if(num%i==0) {
+				int r=num%i;
+				
+				System.out.println("미완성입니다.");
+				break;
+			}else{
+				System.out.println("아님");
+				break;
+			}
+		}
+
+	}
+
+}
