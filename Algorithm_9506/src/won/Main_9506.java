f500cbea5490771b304a318ea675e6290216122e
원보연 <qhdus114@gmail.com>
2019-10-14 11:33:11 +0900

    9506

--- Algorithm_9506/src/won/Main_9506.java
+++ Algorithm_9506/src/won/Main_9506.java
@@ -1,34 +1,28 @@
 package won;
 
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main_9506 {
 	public static void perfectNumber(Integer number) {
 		ArrayList<Integer> pmList = new ArrayList<Integer>();
 		int perfect = 0;
-		for (int i = 1; i <= number; i++) {
+		for (int i = 1; i < number; i++) {
 			if (number % i == 0) {
 				pmList.add(i);
 			}
 		}
 		for (Integer p : pmList) {
-			if (p != number) {
-				perfect += p;
-				System.out.println("p : " + p + " / per : " + perfect);
-			}
+			perfect += p;
 		}
 		if (perfect != number) {
 			System.out.print(number + " is NOT perfect.");
 		} else {
 			System.out.print(number + " = ");
 			for (Integer p : pmList) {
-				if (p != perfect) {
-					System.out.print(p);
-					if (p != perfect - p) {
-						System.out.print(" + ");
-					}
-				} else {
-					break;
+				System.out.print(p);
+				if (p != perfect - p) {
+					System.out.print(" + ");
 				}
 			}
 		}
