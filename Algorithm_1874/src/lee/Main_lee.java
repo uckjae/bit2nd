5e58d9ce96f5af0ae43465f7ca788fde8698d577
욱재 <uckjae@naver.com>
2019-10-28 00:55:08 +0900

    안되네...

--- Algorithm_1874/src/lee/Main_lee.java
+++ Algorithm_1874/src/lee/Main_lee.java
@@ -1,80 +1,83 @@
 package lee;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Stack;
 
-/*
-스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다.
-스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
-1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다.
-이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자.
-임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다.
-이를 계산하는 프로그램을 작성하라.
 
-입력
-첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다.
- 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다.
- 물론 같은 정수가 두 번 나오는 일은 없다.
-
-출력
-입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
-
-예제 입력 1
-8
-4
-3
-6
-8
-7
-5
-2
-1
-예제 출력 1
-+
-+
-+
-+
--
--
-+
-+
--
-+
-+
--
--
--
--
--
- */
 public class Main_lee {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> input = new Queue();
-        Stack<Integer> result = new Stack<>();
-        StringBuilder answer = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int m =0;
-        for(int i=0; i<n;i++){
-            m = Integer.parseInt(br.readLine());
-            input.add(m);
-        }
 
-        int target = input.poll();
-        for(int i=1;i<=target;i++){
-            result.push(i);
-            answer.append("+\n");
+        Queue<Integer> input = new ArrayDeque<>();
+        Queue<Integer> source = new ArrayDeque<>();
+        Queue<Integer> copy = new ArrayDeque<>();
+        Stack<Integer> mystack = new Stack<>();
+        StringBuilder answer = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++){
+            int temp = Integer.parseInt(br.readLine());
+            input.add(temp);
+            copy.add(temp);
+            source.add(i);
         }
-        answer.append("-\n");
-        target = input.poll();
-        if(target<result.peek()){
-            while(target!=result.peek()){
-                result.pop();
-                answer.appen
+        br.close();
+
+        int[] isable = new int[copy.size()];
+        for(int j=0;j<isable.length;j++){
+            isable[j] = copy.poll();
+        }
+        int change = 0;
+        boolean ascend = (isable[0]<isable[1])?true:false;
+        for(int k=0;k<isable.length-1;k++){
+            if(ascend){
+                if(isable[k]>isable[k+1]){
+                    ascend = false;
+                    change++;
+                }
+            }else {
+                if(isable[k]<isable[k+1]){
+                    ascend = true;
+                    change++;
+                }
             }
         }
 
+
+        while(!source.isEmpty()||!mystack.isEmpty()) {
+            if (mystack.isEmpty()) {
+                mystack.push(source.poll());
+
+                answer.append("+");
+            }
+            if (input.peek() == mystack.peek()) {
+                input.poll();
+                mystack.pop();
+                answer.append("\n");
+                answer.append("-");
+            } else if (input.peek() > mystack.peek()) {
+                mystack.push(source.poll());
+                answer.append("\n");
+                answer.append("+");
+            } else {
+                mystack.pop();
+                answer.append("\n");
+                answer.append("-");
+            }
+        }
+        if(change>=3){
+            answer.delete(0,answer.length()-1);
+            answer.append("no");
+        }else if(change==2&&ascend){
+            answer.delete(0,answer.length()-1);
+            answer.append("no");
+        }
+
+
+        System.out.print(answer);
     }
 }
