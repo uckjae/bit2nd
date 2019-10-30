d584275dfd11a71e2bc5c0d15daf4e4614bf4d29
uckjae.lee <uckjae@naver.com>
2019-10-30 21:04:49 +0900

    알고리즘 1463 1만들기

---
+++ makeOne_1463/src/lee/Main.java
@@ -0,0 +1,46 @@
+package lee;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int[] result = new int[1000000];
+
+    public static int mkOne(int n){
+        int num = 0;
+        if(n==1){
+            num = 0;
+        }else if(n<=3){
+            num = 1;
+        }else if(result[n]!=0){
+            num = result[n];
+        }else{
+            if(n%2==0 && n%3==0){
+                if(mkOne(n-1)<=mkOne(n/2) && mkOne(n-1)<=mkOne(n/3)){
+                    result[n] = mkOne(n-1)+1;
+                }else if(mkOne(n/2)<=mkOne(n-1)&&mkOne(n/2)<=mkOne(n/3)){
+                    result[n] = mkOne(n/2)+1;
+                }else {
+                    result[n] = mkOne(n/3)+1;
+                }
+            }else if(n%2==0){
+                result[n]=(mkOne(n-1)<mkOne(n/2))?result[n-1]+1:result[n/2]+1;
+            }else if(n%3==0){
+                result[n]=(mkOne(n-1)<mkOne(n/3))?result[n-1]+1:result[n/3]+1;
+            }else{
+                result[n] = result[n-1]+1;
+            }
+            num = result[n];
+        }
+        return num;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = mkOne(n);
+        System.out.println(answer);
+    }
+}
