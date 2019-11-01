c598501477ef66936a00dc8c8e030ff2ac763bf9
euna.chung <local456a@gmail.com>
2019-11-01 09:22:45 +0900

    밴딩

--- JavaStudy/src/chung/Machine_Main.java
+++ JavaStudy/src/chung/Machine_Main.java
@@ -3,8 +3,8 @@ package chung;
 public class Machine_Main {
 
 	public static void main(String[] args) {
-		vending vm = new vending();
-		//vm.menu();
+		Vending vm = new Vending();
+		vm.menu();
 	}
 
 }
