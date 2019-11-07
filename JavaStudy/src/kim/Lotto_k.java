7b0ab20274bf7e55112f563938a8791ba71af45a
iLGyeom.KIM <electric____@naver.com>
2019-11-07 14:57:00 +0900

    자바 로또 중간저장

--- JavaStudy/src/kim/Lotto_k.java
+++ JavaStudy/src/kim/Lotto_k.java
@@ -13,33 +13,39 @@ import java.util.Scanner;
 
 public class Lotto_k {
 	Scanner sc = new Scanner(System.in);
+	File f = null;
 	FileWriter fw = null;
 	BufferedWriter bfw = null;
 	FileReader fr = null;
 	BufferedReader bfr = null;
 	List<Integer> list = new ArrayList<>();
-	int str = 0;
+	
 	
 	
 	public void Lotto() {
 		while (list.size() < 6) {
-			int lotto = (int) (Math.random() * 45 + 1);
+			int lotto = (int)(Math.random() * 45 + 1);
 			list.add(lotto);
 		}
 
 	}
 
 	public void LottoFile() {
-		
+		String str=" ";
 		Lotto();
 		try {
 			fw = new FileWriter("kim.txt", true); // 경로에 파일 생성하고 , 추가를 허용
 			bfw = new BufferedWriter(fw); // 한줄씩 써서 내려감
-			bfw.write("★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
-			for(int i=0;i<=list.size();i++) {
-				str = list.get(i);
+			bfw.write("★☆★☆★☆★☆★☆");
+			bfw.newLine();
+			for(int i=0;i<list.size();i++) {
+				str += list.get(i) + " ";
+				System.out.println(str);
+				
 			}
+			
 			bfw.write(str);
+			bfw.newLine();
 			fw.flush();
 			bfw.flush();
 			
@@ -48,18 +54,12 @@ public class Lotto_k {
 
 			e.printStackTrace();
 		}finally {
-			try {
-				fw.close();
-				bfw.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			
 		}
 	}
 	
 	public void FileLoad() {
+		f = "kim.txt";
 		try {
 			fr = new FileReader("kim.txt");
 			bfr = new BufferedReader(fr);
@@ -81,40 +81,7 @@ public class Lotto_k {
 	}
 
 	public static void main(String[] args) {
-		FileWriter fw = null;
-		BufferedWriter bfw = null;
-		FileReader fr = null;
-		BufferedReader bfr = null;
-		List<Integer> list = new ArrayList<>();
-		int str = 0;
-		while (list.size() < 6) {
-			int lotto = (int) (Math.random() * 45 + 1);
-			list.add(lotto);
-		}
-		try {
-		fw = new FileWriter("kim.txt", true); // 경로에 파일 생성하고 , 추가를 허용
-		bfw = new BufferedWriter(fw); // 한줄씩 써서 내려감
-		bfw.write("★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆");
-		for(int i=0;i<=list.size();i++) {
-			str = list.get(i);
-		}
-		bfw.write(str);
-		fw.flush();
-		bfw.flush();
-		
-
-	} catch (IOException e) {
-
-		e.printStackTrace();
-	}finally {
-		try {
-			fw.close();
-	
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+		Lotto_k l = new Lotto_k();
+		l.LottoFile();
 	}
 }
