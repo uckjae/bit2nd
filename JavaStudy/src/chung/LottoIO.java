4e4671fe99a9913a2108c3aa0476321bd7f3c661
chungeuna <local456a@gmail.com>
2019-11-09 20:49:33 +0900

    로또 수정

--- JavaStudy/src/chung/LottoIO.java
+++ JavaStudy/src/chung/LottoIO.java
@@ -1,7 +1,8 @@
 package chung;
 
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
@@ -14,44 +15,65 @@ public class LottoIO {
 
 	public List makeLotto() {
 		Set lottoset = new HashSet();
-		
-		for(int i = 0; i < 6; i++) {
-			int num = (int) (Math.random()*45 +1);
+
+		for (int i = 0; i < 6; i++) {
+			int num = (int) (Math.random() * 45 + 1);
 			lottoset.add(num);
-		}	
+		}
 		List list = new LinkedList(lottoset);
 		Collections.sort(list);
 		return list;
 	}
-	
-	public void fileLotto() {
+
+	public void makeFile() {
 		FileWriter fw = null;
 		BufferedWriter bw = null;
 		try {
-			//FileOutputStream fos = new FileOutputStream("LottoIO.txt");
-			//BufferedInputStream bis = new BufferedInputStream(fis);
-			 fw = new FileWriter("LottoIO.txt");
-			 bw = new BufferedWriter(fw);
-	//		byte[] content = makeLotto();
-	//		int data = 0;
-	//		while((data = bos.write()) != -1) {
-	//			bos.write(b);
-	//		}
+			// FileOutputStream fos = new FileOutputStream("LottoIO.txt");
+			// BufferedInputStream bis = new BufferedInputStream(fis);
+			fw = new FileWriter("LottoIO_chung.txt");
+			bw = new BufferedWriter(fw);
+			// byte[] content = makeLotto();
+			// int data = 0;
+			// while((data = bos.write()) != -1) {
+			// bos.write(b);
+			// }
 			bw.write("로또번호");
 			bw.newLine();
 			bw.write(makeLotto().toString());
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				bw.close();
 				fw.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
 		}
-		
-		
 	}
+
+	public void readFile() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("LottoIO_chung.txt");
+			br = new BufferedReader(fr);
+
+			int data = 0;
+			while ((data = fr.read()) != -1) {
+				System.out.print((char) data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- JavaStudy/src/chung/LottoMain.java
+++ JavaStudy/src/chung/LottoMain.java
@@ -3,8 +3,7 @@ package chung;
 public class LottoMain {
 	public static void main(String[] args) {
 		LottoIO lotto = new LottoIO();
-	//	lotto.makeLotto().toString();
-	//	System.out.println(lotto.makeLotto());
-		lotto.fileLotto();
+		lotto.makeFile();
+		lotto.readFile();
 	}
 }
